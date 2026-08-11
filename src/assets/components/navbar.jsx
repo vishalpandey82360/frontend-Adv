9f872e668658818243f0618a3835031541e66999
vishal pandey <vishalpandey82360@gmail.com>
2026-08-11 22:56:12 +0530

    Initial commit

---
+++ src/assets/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className='NAV'>
+      <h2>Myntra</h2>
+      <button id='navbt' type='text'><search>search here</search></button>
+      <div className='navbut'>
+      <Button>HOME</Button>
+      <Button>About</Button>
+      <Button>Features</Button>
+      </div>
+    </div>
+  )
+}
+
+export default navbar
