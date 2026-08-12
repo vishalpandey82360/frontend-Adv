8b3a1a2186b2d3db3b70284412298583a440a14c
vishal pandey <vishalpandey82360@gmail.com>
2026-08-12 23:59:49 +0530

    Add 'vishal' to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+
 
 const navbar = () => {
   return (
     <div className='NAV'>
       <h2>Myntra</h2>
+      <div className='nav-user'>vishal</div>
       <button id='navbt' type='text'><search>search here</search></button>
       <div className='navbut'>
-      <Button>HOME</Button>
-      <Button>About</Button>
-      <Button>Features</Button>
+      <button>HOME</button>
+      <button>About</button>
+      <button>Features</button>
       </div>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import navbar from './assets/components/navbar.jsx'
+import navbar from './components/navbar.jsx'
+<com></com>
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
