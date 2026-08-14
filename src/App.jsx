4e26b6d9e581016df6022fc869d6b7e1e7033113
vishal pandey <vishalpandey82360@gmail.com>
2026-08-14 23:34:30 +0530

    cards created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
 
-const App = () => {
+import React from 'react'
+import Card from    "./components/navbar"
+
+const App = (props) => {
   return (
     <div>
-     
+      <Card user="vishal pandey" age={21}/>
+      <Card user="vishal pandey" age={21}/>
+      <Card user="vishal pandey" age={21}/>
+      <Card user="vishal pandey" age={21}/>
     </div>
   )
 }
 
 export default App
+
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,17 @@
+import React from 'react'
 
-
-const navbar = () => {
+const Card = () => {
   return (
-    <div className='NAV'>
-      <h2>Myntra</h2>
-      <div className='nav-user'>vishal</div>
-      <button id='navbt' type='text'><search>search here</search></button>
-      <div className='navbut'>
-      <button>HOME</button>
-      <button>About</button>
-      <button>Features</button>
-      </div>
+     <div className='parent'>
+     <div className='card'>
+      <img src='https://plus.unsplash.com/premium_photo-1676692121412-028bfb8371b7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D'></img>
+      <h1>vishal pandey</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quas porro ullam sunt nulla dolorum eius. Laudantium, amet tempore</p>
+      <button>view</button>
+    </div>
     </div>
   )
 }
 
-export default navbar
+export default Card
+
