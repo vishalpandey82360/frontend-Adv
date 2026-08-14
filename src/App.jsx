40a7bf219bfd67a991160e01ee78e1308eaec449
vishal pandey <vishalpandey82360@gmail.com>
2026-08-14 23:51:58 +0530

    card num

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@
 import React from 'react'
 import Card from    "./components/navbar"
 
-const App = (props) => {
+const App = () => {
   return (
     <div>
-      <Card user="vishal pandey" age={21}/>
-      <Card user="vishal pandey" age={21}/>
-      <Card user="vishal pandey" age={21}/>
-      <Card user="vishal pandey" age={21}/>
+      <Card user="vishal pandey"  img="https://plus.unsplash.com/premium_photo-1676692121412-028bfb8371b7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D"/>
+      <Card user="tanmay ji"  img="https://images.unsplash.com/photo-1786148270993-f610fd5c8bc1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D"/>
+      <Card user="artic images"  img="https://images.unsplash.com/photo-1785088560047-754af54472d6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMTd8fHxlbnwwfHx8fHw%3D"/>
+      <Card user="londer pink"  img="https://plus.unsplash.com/premium_photo-1738772657819-8f58f03017b7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMTJ8fHxlbnwwfHx8fHw%3D"/>
     </div>
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
   return (
      <div className='parent'>
      <div className='card'>
-      <img src='https://plus.unsplash.com/premium_photo-1676692121412-028bfb8371b7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D'></img>
-      <h1>vishal pandey</h1>
+      <img src={props.img}></img>
+      <h1>{props.user}</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quas porro ullam sunt nulla dolorum eius. Laudantium, amet tempore</p>
       <button>view</button>
     </div>
