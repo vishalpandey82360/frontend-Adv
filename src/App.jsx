079a313149a2678e213612d68f8359a48ad8bb37
vishal pandey <vishalpandey82360@gmail.com>
2026-08-15 23:13:49 +0530

    Update code

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,10 @@ const App = () => {
       <Card user="tanmay ji"  img="https://images.unsplash.com/photo-1786148270993-f610fd5c8bc1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D"/>
       <Card user="artic images"  img="https://images.unsplash.com/photo-1785088560047-754af54472d6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMTd8fHxlbnwwfHx8fHw%3D"/>
       <Card user="londer pink"  img="https://plus.unsplash.com/premium_photo-1738772657819-8f58f03017b7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMTJ8fHxlbnwwfHx8fHw%3D"/>
+      <Card user="hey ji"  img="https://images.unsplash.com/photo-1786723221846-1b4f27748243?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMXx8fGVufDB8fHx8fA%3D%3D"/>
+      <Card/>
     </div>
+    
   )
 }
 
